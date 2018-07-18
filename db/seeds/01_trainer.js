57b89c1aee0c609a4c93627d8231fce8f6e5dcf8
Tim Kozemzak <timkozemzak@gmail.com>
2018-07-18 14:46:38 -0700

    back buttons added

--- db/seeds/01_trainer.js
+++ db/seeds/01_trainer.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('trainer').insert([
 
-        {name: 'Billy Joel', email: 'billy@billyjoel.com', password: 'billyboy', img_url: "https://static1.squarespace.com/static/534b1517e4b0b2883888bafe/t/55777e33e4b07cb18d9e3e5f/1433942663680/Personal-Trainer-in-Chicago.jpg?format=2500w"}
+        {name: 'Billy Joel', email: 'billy@billyjoel.com', password: 'billyboy', bio: 'edit bio here'img_url: "https://static1.squarespace.com/static/534b1517e4b0b2883888bafe/t/55777e33e4b07cb18d9e3e5f/1433942663680/Personal-Trainer-in-Chicago.jpg?format=2500w"}
       ]);
     });
 };
