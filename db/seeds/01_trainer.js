552f1cb383e1686ddb73e8f6df45d06f5dda93bf
Tim Kozemzak <timkozemzak@gmail.com>
2018-07-18 14:59:31 -0700

    edited bio

--- db/seeds/01_trainer.js
+++ db/seeds/01_trainer.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('trainer').insert([
 
-        {name: 'Billy Joel', email: 'billy@billyjoel.com', password: 'billyboy', bio: 'edit bio here', img_url: "https://static1.squarespace.com/static/534b1517e4b0b2883888bafe/t/55777e33e4b07cb18d9e3e5f/1433942663680/Personal-Trainer-in-Chicago.jpg?format=2500w"}
+        {name: 'Billy Joel', email: 'billy@billyjoel.com', password: 'billyboy', bio: 'Billy is an ex Navy Seal who has trained for years to keep his body in top physical shape. He has a degree in nutrition and utilizes his expertise in this area to help bring your body to the next level', img_url: "https://static1.squarespace.com/static/534b1517e4b0b2883888bafe/t/55777e33e4b07cb18d9e3e5f/1433942663680/Personal-Trainer-in-Chicago.jpg?format=2500w"}
       ]);
     });
 };
